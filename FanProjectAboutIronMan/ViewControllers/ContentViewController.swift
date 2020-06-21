309c3566508b57d571c48b914a9dadc597e1a77e
outsider1703 <simka-simka020697simka-simka1>
2020-06-22 00:28:34 +0200

    last commit

--- FanProjectAboutIronMan/ViewControllers/ContentViewController.swift
+++ FanProjectAboutIronMan/ViewControllers/ContentViewController.swift
@@ -9,21 +9,32 @@
 import UIKit
 
 class ContentViewController: UIViewController {
-
+    
     @IBOutlet var contentLabel: UILabel!
     
     private let jsonURL = "https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/id/346.json"
     private var contents: SuperHero!
     
     var selection: String!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
         
+        settingsBackgroundImage()
+    }
+
+    
     func fetchData() {
         guard let url = URL(string: jsonURL) else { return }
         
-        URLSession.shared.dataTask(with: url) { (data, _, _) in
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            if let error = error { print(error); return }
+            if let response = response { print(response) }
+            
             guard let data = data else { return }
             
             let decoder = JSONDecoder()
+            
             do {
                 self.contents = try decoder.decode(SuperHero.self, from: data)
                 DispatchQueue.main.async {
@@ -32,48 +43,72 @@ class ContentViewController: UIViewController {
             } catch  let error {
                 print(error.localizedDescription)
             }
+            
         }.resume()
     }
 }
-
+//MARK: - Private Function
 extension ContentViewController {
+    private func settingsBackgroundImage() {
+        let backgroundImage = UIImageView(frame: UIScreen.main.bounds)
+        backgroundImage.image = UIImage(named: "backgroundT")
+        backgroundImage.contentMode = UIView.ContentMode.scaleToFill
+        self.view.insertSubview(backgroundImage, at: 0)
+        
+        let blurredView = UIVisualEffectView(effect: UIBlurEffect(style: .systemMaterial))
+        blurredView.frame = view.bounds
+        blurredView.alpha = 0.5
+        view.insertSubview(blurredView, at: 1)
+    }
+    
     private func transitionSelection() {
         switch selection {
         case "powerstats":
             contentLabel.text = """
             Powerstats:
+            
             Intelligence: \(contents.powerstats?.intelligence ?? 0)
             Strength: \(contents.powerstats?.strength ?? 0)
             Speed: \(contents.powerstats?.speed ?? 0)
             Durability: \(contents.powerstats?.durability ?? 0)
             Power: \(contents.powerstats?.power ?? 0)
             Combat: \(contents.powerstats?.combat ?? 0)
-"""
+            """
         case "apperance":
             contentLabel.text = """
             Apperance:
+            
             Gender: \(contents.appearance?.gender ?? "")
             Race: \(contents.appearance?.race ?? "")
             Height: \(contents.appearance?.height?.last ?? "")
             Weight: \(contents.appearance?.weight?.last ?? "")
             EyeColor:\(contents.appearance?.eyeColor ?? "")
             HairColor:\(contents.appearance?.hairColor ?? "")
-"""
+            """
         case "biography":
             contentLabel.text = """
             Biography:
+            
             FullName: \(contents.biography?.fullName ?? "")
+            
             AlterEgos: \(contents.biography?.alterEgos ?? "")
+            
             Aliases: \(contents.biography?.aliases?.last ?? "")
+            
             PlaceOfBirth: \(contents.biography?.placeOfBirth ?? "")
+            
             FirstAppearance: \(contents.biography?.firstAppearance ?? "")
+            
             Publisher: \(contents.biography?.publisher ?? "")
+            
             Occupation: \(contents.work?.occupation ?? "")
+            
             Base: \(contents.work?.base ?? "")
+            
             GroupAffiliation: \(contents.connections?.groupAffiliation ?? "")
+            
             Relatives: \(contents.connections?.relatives ?? "")
-
-"""
+            """
         default:
             break
         }
--- FanProjectAboutIronMan/ViewControllers/MainCollectionViewController.swift
+++ FanProjectAboutIronMan/ViewControllers/MainCollectionViewController.swift
@@ -9,8 +9,9 @@
 import UIKit
 
 class MainCollectionViewController: UICollectionViewController {
-
+    
     let categoryNames = CategoryName.allCases
+    private let imageForCell = ["imageForCellOne", "imageForCellTwo", "imageForCellThree"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,28 +19,28 @@ class MainCollectionViewController: UICollectionViewController {
         let image = UIImage(named: "backgroundT")
         let imageView = UIImageView(image: image)
         collectionView.backgroundView = imageView
-
+        
     }
-
+    
     // MARK: UICollectionViewDataSource
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         categoryNames.count
     }
-
+    
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! CollectionViewCell
-    
+        
         let categoryName = categoryNames[indexPath.item]
         cell.categoryLabel.text = categoryName.rawValue
         
-        cell.categoryImage.image = UIImage(named: "imageForCellOne")
-    
+        cell.categoryImage.image = UIImage(named: imageForCell[indexPath.item])
+
         return cell
     }
-
+    
     // MARK: UICollectionViewDelegate
     override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-    let userAction = categoryNames[indexPath.item]
+        let userAction = categoryNames[indexPath.item]
         
         switch userAction {
         case .powerstats:
@@ -53,7 +54,7 @@ class MainCollectionViewController: UICollectionViewController {
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         let contentVC = segue.destination as! ContentViewController
-
+        
         switch segue.identifier {
         case "powerstats":
             contentVC.selection = "powerstats"
@@ -68,7 +69,6 @@ class MainCollectionViewController: UICollectionViewController {
             break
         }
     }
-
 }
 
 
